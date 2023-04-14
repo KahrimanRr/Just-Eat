1632037dd35e4edde7ef821f106cc6e0c4369f94
Rijad Kahriman <rijadkahr@gmail.com>
2023-04-14 03:08:53 +0200

    added items on the navbar and usestate

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,17 @@
-import React from "react";
-import { AiOutlineMenu, AiOutlineSearch } from "react-icons/ai";
+import React, { useState } from "react";
+import { AiOutlineMenu, AiOutlineSearch, AiOutlineClose } from "react-icons/ai";
 import { BsFillCartFill } from "react-icons/bs";
+import { TbTruckDelivery } from "react-icons/tb";
+import { FaWallet } from "react-icons/fa";
+import { MdFavorite, MdHelp } from "react-icons/md";
 const Navbar = () => {
+  const [sidebar, SetSidebar] = useState(false);
+
   return (
     <div className="max-w-[1640px] mx-auto flex justify-between items-center p-4">
       {/* Left side */}
       <div className="flex items-center">
-        <div className="cursor-pointer">
+        <div onClick={() => SetSidebar(!sidebar)} className="cursor-pointer">
           <AiOutlineMenu size={30} />
         </div>
         <h1 className="text-2xl sm:text-3xl lg:text-4xl px-2">
@@ -33,6 +38,45 @@ const Navbar = () => {
       <button className="bg-green-500 text-green-100 hidden md:flex items-center py-2 rounded-full ">
         <BsFillCartFill size={27} />
       </button>
+
+      {/*/**mobile */}
+      {sidebar ? (
+        <div className="bg-black/80 fixed w-full h-screen z-10 top-0 left-0"></div>
+      ) : (
+        ""
+      )}
+
+      {/*sidemenu*/
+      /** */}
+      <div
+        className={
+          sidebar
+            ? "fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300"
+            : "fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-300"
+        }
+      >
+        <AiOutlineClose
+          onClick={() => SetSidebar(!sidebar)}
+          size={30}
+          className="absolute  right-4 top-4 cursor-pointer"
+        />
+        <nav>
+          <ul className="flex flex-col p-4 text-grey-800">
+            <li className="text-xl py-4 px-4 mt-6 flex">
+              <TbTruckDelivery size={25} className="mr-4" /> Orders
+            </li>
+            <li className="text-xl py-4 px-4 mt-6 flex">
+              <MdFavorite size={25} className="mr-4" /> Favorites
+            </li>
+            <li className="text-xl py-4 px-4 mt-6 flex">
+              <FaWallet size={25} className="mr-4" /> Wallet
+            </li>
+            <li className="text-xl py-4 px-4 mt-6 flex">
+              <MdHelp size={25} className="mr-4" /> Help
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 };
