50efb07cb9f94cca282469243da3b757047e0676
Rijad Kahriman <rijadkahr@gmail.com>
2023-05-06 17:27:17 +0200

    new images

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -9,60 +9,56 @@ const Cards = () => {
       <div className=" rounded-xl relative ">
         {/**overlay */}
         <div className="absolute w-full h-full bg-black/30 rounded xl text-white">
-          <p className="font-bold text-2xl px-2 pt-4">sun is out </p>
-          <p className="px-2">through</p>
+          <p className="font-bold text-2xl px-2 pt-4">Appetizer </p>
           <button className=" border-white bg-white text-black mx-2 absolute bottom-4">
             order now
           </button>
         </div>
         <img
           className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
-          src="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Zm9vZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=400&q=60"
+          src="https://images.unsplash.com/photo-1606755456206-b25206cde27e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGFwcGV0aXplcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
           alt=""
         />
       </div>
       <div className=" rounded-xl relative ">
         {/**overlay */}
         <div className="absolute w-full h-full bg-black/30 rounded xl text-white">
-          <p className="font-bold text-2xl px-2 pt-4">sun is out </p>
-          <p className="px-2">through</p>
+          <p className="font-bold text-2xl px-2 pt-4">Main dish </p>
           <button className=" border-white bg-white text-black mx-2 absolute bottom-4">
             order now
           </button>
         </div>
         <img
           className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
-          src="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Zm9vZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=400&q=60"
+          src="https://images.unsplash.com/photo-1572448910681-3341d74893d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8bWFpbiUyMGRpc2h8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
           alt=""
         />
       </div>
       <div className=" rounded-xl relative ">
         {/**overlay */}
         <div className="absolute w-full h-full bg-black/30 rounded xl text-white">
-          <p className="font-bold text-2xl px-2 pt-4">sun is out </p>
-          <p className="px-2">through</p>
+          <p className="font-bold text-2xl px-2 pt-4">Dessert</p>
           <button className=" border-white bg-white text-black mx-2 absolute bottom-4">
             order now
           </button>
         </div>
         <img
           className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
-          src="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Zm9vZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=400&q=60"
+          src="https://images.unsplash.com/photo-1603532648955-039310d9ed75?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjJ8fGRlc3NlcnR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
           alt=""
         />
       </div>
       <div className=" rounded-xl relative ">
         {/**overlay */}
         <div className="absolute w-full h-full bg-black/30 rounded xl text-white">
-          <p className="font-bold text-2xl px-2 pt-4">sun is out </p>
-          <p className="px-2">through</p>
+          <p className="font-bold text-2xl px-2 pt-4">Drinks </p>
           <button className=" border-white bg-white text-black mx-2 absolute bottom-4">
             order now
           </button>
         </div>
         <img
           className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
-          src="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Zm9vZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=400&q=60"
+          src="https://images.unsplash.com/photo-1513558161293-cdaf765ed2fd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZHJpbmt8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
           alt=""
         />
       </div>
